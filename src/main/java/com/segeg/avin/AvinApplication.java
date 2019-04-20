a26e7509218841a202bf2d69de8ca8ec7b0765c2
iinow <iinow@naver.com>
2019-04-20 10:25:33 +0900

    add test code

--- src/main/java/com/segeg/avin/AvinApplication.java
+++ src/main/java/com/segeg/avin/AvinApplication.java
@@ -2,6 +2,11 @@ package com.segeg.avin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AvinApplication {
@@ -9,5 +14,4 @@ public class AvinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AvinApplication.class, args);
 	}
-
 }
--- src/main/java/com/segeg/avin/controller/AvController.java
+++ src/main/java/com/segeg/avin/controller/AvController.java
@@ -1,9 +1,11 @@
 package com.segeg.avin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1/av")
+@RestController
+@RequestMapping("/v1/av")
 public class AvController {
 
     @GetMapping("/hello")
