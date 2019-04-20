930e19cdc69cae670315c4d985cb643d81aaf769
iinow <iinow@naver.com>
2019-04-20 10:10:14 +0900

    add test code

--- src/main/java/com/segeg/avin/controller/AvController.java
+++ src/main/java/com/segeg/avin/controller/AvController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/v1/av")
 public class AvController {
-    
+
     @GetMapping("/hello")
     public String hello(){
         return "hello";
